5260d206027a7229d583ddd70027eef77188dbc2
Bob Caddy <r.Caddy@pitt.edu>
2023-01-03 14:53:54 -0500

    Revert "update gemfile.lock"
    This reverts commit 4b8bb7d4a8a0d58986cc043e21b536d947f5446d.

--- local-build.sh
+++ local-build.sh
@@ -9,12 +9,12 @@ if [[ $1 == "run" ]]; then
     # Run the website with auto update on
     set -m
     bundle exec jekyll s -H 0.0.0.0 -l -open-url
-    bundle exec jekyll clean
+    jekyll clean
 
 elif [[ $1 == "test" ]]; then
     bundle exec jekyll b -d "_site"
     bundle exec htmlproofer _site --disable-external --check-html --allow_hash_href
-    bundle exec jekyll clean
+    jekyll clean
 
 else
     echo "Command not found. Please only use \"run\" or \"test\" "
