4b8bb7d4a8a0d58986cc043e21b536d947f5446d
Bob Caddy <r.Caddy@pitt.edu>
2023-01-03 14:51:30 -0500

    update gemfile.lock

--- local-build.sh
+++ local-build.sh
@@ -9,12 +9,12 @@ if [[ $1 == "run" ]]; then
     # Run the website with auto update on
     set -m
     bundle exec jekyll s -H 0.0.0.0 -l -open-url
-    jekyll clean
+    bundle exec jekyll clean
 
 elif [[ $1 == "test" ]]; then
     bundle exec jekyll b -d "_site"
     bundle exec htmlproofer _site --disable-external --check-html --allow_hash_href
-    jekyll clean
+    bundle exec jekyll clean
 
 else
     echo "Command not found. Please only use \"run\" or \"test\" "
