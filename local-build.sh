7c2c3daaf75aea51c646b9bf2ccb1ef018689e0d
Bob Caddy <r.Caddy@pitt.edu>
2024-06-17 11:29:29 -0400

    Update to v7.0.1

--- local-build.sh
+++ local-build.sh
@@ -13,7 +13,7 @@ if [[ $1 == "run" ]]; then
 
 elif [[ $1 == "test" ]]; then
     bundle exec jekyll b -d "_site"
-    bundle exec htmlproofer _site --disable-external=true --ignore-urls "/^http:\/\/127.0.0.1/,/^http:\/\/0.0.0.0/,/^http:\/\/localhost/"
+    bundle exec htmlproofer _site --disable-external --ignore-urls "/^http:\/\/127.0.0.1/,/^http:\/\/0.0.0.0/,/^http:\/\/localhost/"
     jekyll clean
 
 elif [[ $1 == "update" ]]; then
